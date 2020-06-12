58becb503d6b5e5e351b22ff5b2067964012d796
James Carman <jcarman@callibrity.com>
2020-06-12 16:37:39 -0400

    Working ConfigMap.

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
+    @Value("${hello.greeting}")
+    private String greeting;
+
     @GetMapping
     public String sayHello() {
-        return "Hello, Spring-K8S!";
+        return greeting;
     }
 }
